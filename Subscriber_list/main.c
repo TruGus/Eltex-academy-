f94953656cb09c4dbbf45c43e7b6501c61ae9f73
kostya <kgaczko@mail.ru>
2024-03-12 19:36:47 +0700

    Изменения, которые будут включены в коммит: 	новый файл:    DynamicMemoryList/Makefile 	новый файл:    DynamicMemoryList/test

---
+++ Subscriber_list/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+ struct abonent{
+  char name[10];
+  char second_name[10];
+  char tel[10];
+};
+
+int main() {
+ char choose;
+ while(1){
+ printf("Выберите опцию\n 1 - добавить абонента \n 2 - удалить абонента \n 3 - поиск абонента \n 4 - Вывести список\n 5 - Выход \n");
+
+ scanf("%d", &choose);
+ switch(choose){
+ }
+  printf("Выход.........");
+  return 0;
+ }
+}
+
